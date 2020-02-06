51e38aa184b62a90064aaaa6778801fbe039e60e
TyomaMtl <scollioudfree.fr@gmail.com>
2020-02-06 10:19:44 +0100

    refactor : examples

--- example/public/index.php
+++ example/public/index.php
@@ -3,18 +3,18 @@
 require '../../vendor/autoload.php';
 
 // use App\Entity\Film;
-use App\Repository\FilmRepository;
+// use App\Repository\FilmRepository;
 
 // $film = new Film();
-// $film->setTitle('New film');
+// $film->setTitle('Pulpe fiction');
 // $film->save();
 
-$films = FilmRepository::getRepository();
+// $films = FilmRepository::getRepository();
 
-// $myFilm = $films->getById('12');
-// echo $myFilm->getTitle();
-// $myFilm->setTitle("Jean jean");
+// $myFilm = $films->getById('13');
+// // echo $myFilm->setTitle();
+// $myFilm->setTitle("Star Wars");
 // $myFilm->save();
 
-$mySecondFilm = $films->getById('8');
-$mySecondFilm->delete();
+// $mySecondFilm = $films->getById('13');
+// $myFilm->delete();
